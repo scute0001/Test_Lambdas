aa6ad2d6e21cdd7c2f1828c218035234caf3cdd1
Scute0001 <s12t8811@gmail.com>
2020-09-04 16:12:08 +0800

    add GroceryLambdaTest

--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -1,4 +1,39 @@
+typealias DoubleConversion = (Double) -> Double
+
+//fun convert(x:Double,
+//            converter: (Double) -> Double) : Double {
+//    val result = converter(x)
+//    println("$x is converted to $result")
+//    return result
+//}
+
+fun convert(x:Double,
+            converter: DoubleConversion) : Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun getConversionLambda(str: String): DoubleConversion {
+    if (str == "CentigradeToFahrenheit"){
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds"){
+        return { it * 2.204623 }
+    } else if (str == "PoundsToUSTons"){
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
+
+fun combine(lambda1: DoubleConversion,
+            lambda2: DoubleConversion): DoubleConversion {
+    return { x:Double -> lambda2(lambda1(x)) }
+}
+
 fun main(args: Array<String>){
+
+
 //    var addFive = { x: Int -> x + 5 }
 //    println("Pass 6 to addFive: ${addFive(6)}")
 //
@@ -14,20 +49,25 @@ fun main(args: Array<String>){
 //
 //    val myLambda: () -> Unit = { println("Hi") }
 //    myLambda()
-    fun convert(x:Double,
-                converter: (Double) -> Double) : Double {
-        val result = converter(x)
-        println("$x is converted to $result")
-        return result
-    }
 
-    fun convertFive(converter: (Int) -> Double) : Double {
-        val result = converter(5)
-        println("5 is converted to $result")
-        return result
-    }
-    convert(20.0) { it * 1.8 + 32}
+
+//    fun convertFive(converter: (Int) -> Double) : Double {
+//        val result = converter(5)
+//        println("5 is converted to $result")
+//        return result
+//    }
+//    convert(20.0) { it * 1.8 + 32}
 //    convertFive { x -> x * 1.8 + 32 }
 
+//    2.5kg to pounds
+    println("Convert 2.5kg to Pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
 
+//    Difinition two convert Lambdas
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+//    Combine tow lambdas
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundToUSTonsLambda)
+//    use new change 17.4kgs to USTons
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US Tons")
 }
